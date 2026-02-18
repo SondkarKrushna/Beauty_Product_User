f79e345e8eebf4536af5fa02edd36fad317fb81e
Krushna Sondkar <sondkarkrushna@gmail.com>
2026-02-18 15:25:25 +0530

    commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  
+  const token = localStorage.getItem("beauty_token");
+
 
   if (!token) {
     return <Navigate to="/" replace />;
